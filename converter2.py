c51456a51408e0ce8b896be9f294ec4b96d1a129
vvdkhoa <52860495+vvdkhoa@users.noreply.github.com>
2021-04-09 19:40:45 +0900

    Update converter2.py

--- converter2.py
+++ converter2.py
@@ -1,44 +1,37 @@
-# Read setting ##########################
-import json
-with open('settings.json') as json_file:
-    json_str = json_file.read()
-    my_settings = json.loads(json_str)#[0]
-
-# Root Path Setting #####################
 import os
 import sys
+import shutil
+import ntpath
+
+# Root Path Setting #####################
 root_path = os.getcwd()
-#print('Root Path: ' + str(root_path))
 sys.path.insert(1, root_path)
 
-
-# Default CAD Directory Setting #########
-if my_settings['output_path'] == '':
-    my_settings['output_path'] = str(os.path.join(root_path, 'CAD_DATA/OutputFile'))
-if my_settings['processing_path'] == '':
-    my_settings['processing_path'] = str(os.path.join(root_path, 'CAD_DATA/ProcessingFile'))
-
+# Import HELP function
+from my_help import my_print, read_my_settings, absoluteFilePaths
 
 # Import FreeCad Library
+my_settings = read_my_settings()
 sys.path.append(my_settings['FreeCAD_path'])
 import FreeCAD
 import Mesh
 import Part
 
 
-# Import HELP function
-from my_help import my_print
-
-
 #########################################
 # FreeCad: STL => IGES, STEP
-def convert_stl_iges(my_settings):
+def convert_stl_iges(file_path):
 
     my_print('Convert Data')
 
+    my_settings = read_my_settings()
+
+    # Create temporary file
+    tem_file_path = "{}\\FreeCadTemporary\\filtered_mesh.stl".format(my_settings['processing_path'])
+    shutil.copyfile(file_path, tem_file_path)
+
     # Read STL
-    input_path = "{}\\filtered_mesh.stl".format(my_settings['processing_path'])
-    Mesh.open(u"{}".format(input_path))
+    Mesh.open(u"{}".format(tem_file_path))
     App.setActiveDocument("Unnamed") #
     App.ActiveDocument=App.getDocument("Unnamed") #
 
@@ -66,15 +59,40 @@ def convert_stl_iges(my_settings):
         __objs__.append(FreeCAD.getDocument("Unnamed").getObject("filtered_mesh001"))  # If only surface available
 
     # Export
+    input_file_name = ntpath.basename(file_path).split('.')[0]
+
     for file_type in my_settings['export_file_type']:
-        save_path = "{}\\sample.{}".format(my_settings['output_path'], file_type)
+        save_path = "{}\\{}.{}".format(my_settings['output_path'], input_file_name, file_type)
         my_print("Export {} File :\br{}".format(file_type, save_path))
 
         Part.export(__objs__,u"{}".format(save_path))
 
+
+##############################################
+# Get data which fillted by Meshlap
+def get_processing_file():
+
+    # Read Input path
+    my_settings = read_my_settings()
+    input_path = my_settings['processing_path']
+
+    all_files = absoluteFilePaths(input_path)
+
+    input_files = []
+    for file in all_files:
+        file_type = os.path.splitext(file)[1]
+        if os.path.splitext(file)[1] in ['.stl']:
+            input_files.append(file)
+    return input_files
+
+
 #########################################
 if __name__ == '__main__':
 
-    convert_stl_iges(my_settings)
+    # Get data which fillted by Meshlap
+    processing_files = get_processing_file()
+
+    for file_path in processing_files:
+        convert_stl_iges(file_path)
 
     my_print('Convert STL To IGES, STEP completed.')
